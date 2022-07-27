d5aa26e15f9eaa63b4434964a4ca9e359d58b35c
Manojes <manoj.esw@gmail.com>
2022-07-27 17:45:24 +0530

    2 files modified, 1 added

--- src/User.js
+++ src/User.js
@@ -1,26 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const User = () => {
-const addUserHandler=(event)=>{
-event.preventDefault();
-  }
-  
+  const [enteredUsername, setenteredUsername] = useState('');
+  const [enteredage, setenteredage] = useState('');
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    setenteredUsername('');
+    setenteredage('');
+  };
+
+  const userNameChangeHandler = (event) => {
+    setenteredUsername(event.target.value);
+  };
+
+  const ageHandler = (event) => {
+    setenteredage(event.target.value);
+  };
+
   return (
-    <div class="col-lg-offset-4" >
-
+    <div class="col-lg-offset-4">
       <form onSubmit={addUserHandler}>
-      <div class="p-2  col-md-6">
-      <label class="p-1" htmlfor="username"> Username </label>
-        <input class="form-control" id="username" type="text" />
-      </div>
+        <div class="p-2  col-md-6">
+          <label class="p-1" htmlfor="username">
+            {' '}
+            Username{' '}
+          </label>
+          <input
+            class="form-control"
+            id="username"
+            type="text"
+            onchange={userNameChangeHandler}
+          />
+        </div>
 
-      <div class="p-2 col-md-6 ">
-        <label class="p-1" htmlfor="age"> Age </label>
-        <input class="form-control" id="age" type="text" />
-      </div>
+        <div class="p-2 col-md-6 ">
+          <label class="p-1" htmlfor="age">
+            {' '}
+            Age{' '}
+          </label>
+          <input
+            class="form-control"
+            id="age"
+            type="text"
+            onchange={ageHandler}
+          />
+        </div>
 
-      <div onClick={} className="btn btn-primary"> button </div>
-
+        <div className="btn btn-primary"> button </div>
       </form>
     </div>
   );
